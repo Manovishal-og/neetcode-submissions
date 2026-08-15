f733d2d5abb14cfe0bb59098418515717f84bf4e
Manovishal-og <154641976+Manovishal-og@users.noreply.github.com>
2026-08-15 22:09:41 +0530

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.java
@@ -0,0 +1,74 @@
+class MyHashSet {
+
+    private class Node{
+        int key ;
+        Node next;
+
+        public Node(int key ){
+            this.key = key;
+        }
+    }
+    private static Node[] buck;
+
+    private static final int SIZE = 1000;
+
+    public MyHashSet() {
+        buck = new Node[SIZE];
+    }
+    
+    public Boolean contains(int key){
+        int in = hash(key);
+        Node cur = buck[in];
+        while(cur != null){
+            if(cur.key == key) return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    public void add(int key) {
+        int in = hash(key);
+        Node cur = buck[in];
+        while(cur != null){
+            if(cur.key == key) return;
+            cur = cur.next;
+        }
+        Node ne = new Node(key);
+        ne.next = buck[in];
+        buck[in] = ne;
+    }
+    
+    public void remove(int key) {
+        int in = hash(key);
+        Node prev = null ;
+        Node cur = buck[in];
+        
+        while( cur != null){
+            if(cur.key == key){
+                if(prev == null)
+                    buck[in] = cur.next;
+                else
+                    prev.next = cur.next;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        
+
+        
+    }
+
+    private int hash(int key){
+        return key % SIZE;
+    }
+
+    
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */

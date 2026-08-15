5b61026066345bda5890ca259fd069ea33801301
Manovishal-og <154641976+Manovishal-og@users.noreply.github.com>
2026-08-15 22:21:50 +0530

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int e =  s.length() - 1;
+        while(i < e){
+            if(s.charAt(i) != s.charAt(e))
+                return isP( s,i+1 , e ) || isP(s,i,e-1);
+            i++;
+            e--;
+        }
+
+        
+        return true;
+    }
+
+    public boolean isP(String s , int l , int r){
+        while(l < r){
+            if(s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
